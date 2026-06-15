f61302ed5266f27c1aeee2c2123b57773c9cc731
lucas carioca <69126344+Lucas3435@users.noreply.github.com>
2026-06-15 15:28:26 -0300

    Add word tracker to count word occurrences in text
    Implement a word tracker that counts word occurrences in a given text using dictionaries and displays results sorted by count.

---
+++ faculdade1/tracker de palavra.py
@@ -0,0 +1,16 @@
+#um tracker de palavras, que conta quantas vezes cada palavra aparece em um texto,  utiliazando de dicionarios para armazenar as palavras e suas contagens, e depois exibe os resultados.
+#fazendo uso de ferramentas para separar as chaves e os valores, e para ordenar os resultados de forma clara. sendo a saida uma lista que apresente
+#cada palavra seguida de sua contagem, da maior para a menor contagem.
+
+texto = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non sapien et neque laoreet cursus at at nisl.
+Vestibulum ut lacus sem. Suspendisse in sapien purus. Proin nec rhoncus orci. Mauris dapibus ex urna, non dapibus risus scelerisque in.
+Praesent quis egestas metus, vitae sollicitudin turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Duis odio massa, pulvinar eu maximus vitae, imperdiet non est. Fusce ultricies fringilla nunc eu fringilla.
+Etiam eget tempor risus, non dictum mauris.
+Vivamus quis elit commodo, fringilla libero in, convallis sem. Aenean porttitor gravida dolor, et faucibus ex egestas eget.
+Cras fermentum pretium rutrum. Aliquam at lectus velit. Mauris tristique nec tortor nec commodo.
+Etiam euismod finibus imperdiet. Praesent lobortis sodales tortor, non porttitor nunc fermentum eu. Nam varius a odio non porttitor.
+Aenean pellentesque urna vel ante lobortis, tristique congue lorem auctor. Curabitur justo arcu, venenatis quis pulvinar in, ultrices aliquet justo.
+Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam erat volutpat. Maecenas egestas elit arcu, vel accumsan eros suscipit nec.
+Donec orci diam, dignissim vitae mollis ut, venenatis nec justo. Nulla auctor non tortor nec vehicula. Suspendisse mattis libero at lectus volutpat auctor.
+Mauris eget pulvinar ante."
